ed0e7a4507d75f7a44dda2ae70d55efe0d719b58
chakravarthig <gchakravarthia8@gmail.com>
2019-08-30 15:25:42 +0530

    [Sunil|chakravarthi] Add. Area of Rectangle with 0 lenght and width

---
+++ src/main/java/com/thoughtworks/geometory/Rectangles.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.geometory;
+
+public class Rectangles {
+
+    private float length;
+    private float width;
+    Rectangles(float lenght,float width){
+        this.length=lenght;
+        this.width=width;
+    }
+
+     float area(){
+        return 0.0f;
+    }
+}
