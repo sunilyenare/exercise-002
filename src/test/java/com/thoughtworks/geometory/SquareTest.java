87bcc3eda43a2f2bf201bc20f47b70ac3b3b6d49
sunilsyenare <syenare97@gmail.com>
2019-09-03 14:44:17 +0530

    [satya|sunil] Add.calculate area of square when size Two

--- src/test/java/com/thoughtworks/geometory/SquareTest.java
+++ src/test/java/com/thoughtworks/geometory/SquareTest.java
@@ -5,10 +5,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SquareTest {
+
     @Test
     void givenZeroSizeSquare_WhenCalculateArea_thenShouldReturnZero() {
         float size = 0.0f;
         RectangleOrSquare square = ShapeFactory.getSquare(size);
         assertEquals(0.0f, square.area());
     }
+
+    @Test
+    void givenTwoSizeSquare_WhenCalculateArea_thenShouldReturnFour() {
+        float size = 2.0f;
+        RectangleOrSquare square = ShapeFactory.getSquare(size);
+        assertEquals(4.0f, square.area());
+    }
+
 }
